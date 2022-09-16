d032d156953b31be1972847a6d5856f78b6b9a30
Svabas22 <simonas.svabas2002@gmail.com>
2022-09-16 18:47:23 +0300

    Added all widgets

--- app/src/main/java/com/example/wordcounter/MainActivity.java
+++ app/src/main/java/com/example/wordcounter/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.wordcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText txtInput;
+    Spinner countOpt;
+    TextView txtCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.txtInput = findViewById(R.id.txtInput);
+        this.countOpt = findViewById(R.id.spinner);
+        this.txtCount = findViewById(R.id.txtResult);
     }
 }
